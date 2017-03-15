fabbab503518191fce4d663f53b3917915a5ecf8
Jayvee <jayveelugod@gmail.com>
2017-03-15 15:17:19 +0000

    Issues fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20170315104208) do
 
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "picture"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -22,5 +32,5 @@ ActiveRecord::Schema.define(version: 20170315104208) do
     t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-  
-end  
+
+end
